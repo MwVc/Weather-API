bde394b1cdc233fff8b068bd57fb062f8699a47d
Victor Mwadime <victormwadime@gmail.com>
2024-03-05 13:54:08 +0300

    create getWeatherData function

--- src/js/main.js
+++ src/js/main.js
@@ -10,3 +10,12 @@ toolTips.forEach((t) => {
 });
 
 // Fetch Functions
+async function getWeatherData() {
+  const promise = await fetch(
+    "https://api.weatherapi.com/v1/current.json?key=1a5ff743f48f43298ad132819242302&q=london"
+  );
+  const data = await promise.json();
+  console.log(data);
+}
+
+getWeatherData();
