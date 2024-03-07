cff7d32acc0122d4b546e1f7181bfa57bd852e44
Victor Mwadime <victormwadime@gmail.com>
2024-03-07 13:34:51 +0300

    add try catch block and display weather data targeted elements

--- src/js/main.js
+++ src/js/main.js
@@ -7,30 +7,74 @@ import * as bootstrap from "bootstrap";
 (function () {
   const form = document.querySelector("form");
   const searchInput = document.querySelector("#search-input");
-  console.log(searchInput.style);
+  const button = document.getElementById("search-btn");
 
   form.addEventListener("submit", (event) => {
     event.preventDefault();
     if (!(searchInput.value == "")) {
-      getWeatherData();
+      getWeatherData;
+    } else {
+      button.setAttribute("data-bs-toggle", "modal");
+      button.setAttribute("data-bs-target", "#exampleModal");
     }
   });
 })();
 
 async function getWeatherData() {
-  const promise = await fetch(
-    "https://api.weatherapi.com/v1/current.json?key=1a5ff743f48f43298ad132819242302&q=mombasa"
-  );
-  const data = await promise.json();
-  console.log(document.getElementById("locationData"));
-  console.log(document.getElementById("localTimeData"));
-  console.log(document.getElementById("conditionData"));
-  console.log(document.getElementById("feelsLikeData"));
-  console.log(document.getElementById("humidityData"));
-  console.log(document.getElementById("pressureData"));
-  console.log(document.getElementById("temperatureData"));
-  console.log(document.getElementById("visibilityData"));
-  console.log(document.getElementById("windDirectionData"));
-  console.log(document.getElementById("windSpeedData"));
-  console.log(data);
+  try {
+    const promise = await fetch(
+      "https://api.weatherapi.com/v1/current.json?key=1a5ff743f48f43298ad132819242302&q=oidfodsd"
+    );
+    console.log(promise);
+    if (promise.ok) {
+      const weatherData = await promise.json();
+      console.log(weatherData);
+
+      document.getElementById(
+        "locationData"
+      ).innerText = `${weatherData.location.name}, ${weatherData.location.country}`;
+      document.getElementById(
+        "localTimeData"
+      ).innerText = `${weatherData.location.localtime}`;
+      document.getElementById(
+        "conditionData"
+      ).innerText = `${weatherData.current.condition.text}`;
+      document
+        .getElementById("conditionDataIcon")
+        .setAttribute("src", `${weatherData.current.condition.icon}`);
+      document.getElementById(
+        "feelsLikeData"
+      ).innerText = `${weatherData.current.feelslike_c}°C / ${weatherData.current.feelslike_f}°F`;
+      document.getElementById(
+        "humidityData"
+      ).innerText = `${weatherData.current.humidity}%`;
+      document.getElementById(
+        "pressureData"
+      ).innerText = `${weatherData.current.pressure_in}in / ${weatherData.current.pressure_mb}mb`;
+      document.getElementById(
+        "temperatureData"
+      ).innerText = `${weatherData.current.temp_c}°C / ${weatherData.current.temp_f}°F`;
+      document.getElementById(
+        "visibilityData"
+      ).innerHTML = `${weatherData.current.vis_km} km / ${weatherData.current.vis_miles} miles`;
+      document.getElementById(
+        "windDirectionData"
+      ).innerHTML = `${weatherData.current.wind_dir}`;
+      document.getElementById(
+        "windSpeedData"
+      ).innerHTML = `${weatherData.current.wind_kph} kph / ${weatherData.current.wind_mph} mph`;
+    } else {
+      if (promise.status === 401 || promise.status === 403) {
+        throw new Error("Unauthorized: Check your API key or permissions");
+      } else if (promise.status === 400) {
+        throw new Error(
+          "No location found matchin your location or bad request check permissions"
+        );
+      } else {
+        throw new Error("Unknown error. Please try again later");
+      }
+    }
+  } catch (error) {
+    alert(error);
+  }
 }
